866c0ebaad728c4e5c328c4187010fa35bdfaa9a
LyubomirRashkov <lyubomir.rashkov88@gmail.com>
2021-10-27 11:03:09 +0300

    Added code (First lecture in the course Programming Basics)

---
+++ ProgrammingBasicsC#/1lab-FirstStepsInCodding/PetShop/PetShop/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace PetShop
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int dogs = int.Parse(Console.ReadLine());
+            int animals = int.Parse(Console.ReadLine());
+
+            double sum = dogs * 2.50 + animals * 4;
+
+            Console.WriteLine($"{sum} lv.");
+        }
+    }
+}
---
+++ ProgrammingBasicsC#/1lab-FirstStepsInCodding/YardGreening/YardGreening/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace YardGreening
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double squaremeters = double.Parse(Console.ReadLine());
+
+            double fullPrice = squaremeters * 7.61;
+            double discount = 0.18 * fullPrice;
+            double finalPrice = fullPrice - discount;
+
+            Console.WriteLine($"The final price is: {finalPrice} lv.");
+            Console.WriteLine($"The discount is: {discount} lv.");
+        }
+    }
+}
