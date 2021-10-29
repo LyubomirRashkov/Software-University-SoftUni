7934d0bdf8690369b4c612fd5d861fd98b8cd63a
LyubomirRashkov <lyubomir.rashkov88@gmail.com>
2021-10-29 11:18:47 +0300

    Added code (First exercise in the course Programming Basics)

---
+++ ProgrammingBasicsC#/1ex-FirstStepsInCoding/VacationBooksList/VacationBooksList/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace VacationBooksList
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int pages = int.Parse(Console.ReadLine());
+            double pagesPerHour = double.Parse(Console.ReadLine());
+            int allowableNumOfDays = int.Parse(Console.ReadLine());
+
+            double hoursForWholeBook = pages / pagesPerHour;
+            double hoursPerDay = hoursForWholeBook / allowableNumOfDays;
+
+            Console.WriteLine(hoursPerDay);
+        }
+    }
+}
