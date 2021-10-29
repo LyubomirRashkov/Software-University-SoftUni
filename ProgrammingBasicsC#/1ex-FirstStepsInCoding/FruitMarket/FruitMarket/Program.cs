78b6a0a546f31ce015b1b67d7957edf1f34636c6
LyubomirRashkov <lyubomir.rashkov88@gmail.com>
2021-10-29 11:16:43 +0300

    Added code (First exercise in the course Programming Basics)

---
+++ ProgrammingBasicsC#/1ex-FirstStepsInCoding/FruitMarket/FruitMarket/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace FruitMarket
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double strawberriesPrice = double.Parse(Console.ReadLine());
+            double bananas = double.Parse(Console.ReadLine());
+            double oranges = double.Parse(Console.ReadLine());
+            double raspberries = double.Parse(Console.ReadLine());
+            double strawberries = double.Parse(Console.ReadLine());
+
+            double raspberriesPrice = strawberriesPrice * 0.5;
+            double orangesPrice = raspberriesPrice * 0.6;
+            double bananasPrice = raspberriesPrice * 0.2;
+
+            double sum = strawberriesPrice * strawberries + bananasPrice * bananas + orangesPrice * oranges + raspberriesPrice * raspberries;
+
+            Console.WriteLine(Math.Round(sum,2));
+        }
+    }
+}
