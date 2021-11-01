545c5c931ea37dd04094aa6268abd5f085fa1283
LyubomirRashkov <lyubomir.rashkov88@gmail.com>
2021-11-01 09:38:11 +0200

    Added code (Second lecture in the course Programming Basics)

---
+++ ProgrammingBasicsC#/2lab-ConditionalStatements/Excellent Result/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Excellent_Result
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double evaluation = double.Parse(Console.ReadLine());
+
+            if (evaluation >= 5.50)
+            {
+                Console.WriteLine("Excellent!");
+            }
+        }
+    }
+}
