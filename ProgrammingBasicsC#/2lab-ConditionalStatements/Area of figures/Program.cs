d598a93009ff4f898e3866061ef7fc7ae25029fa
LyubomirRashkov <lyubomir.rashkov88@gmail.com>
2021-11-01 09:37:16 +0200

    Added code (Second lecture in the course Programming Basics)

---
+++ ProgrammingBasicsC#/2lab-ConditionalStatements/Area of figures/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace Area_of_figures
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string kindOfFigure = Console.ReadLine();
+
+            double area = 0.000;
+
+            if (kindOfFigure == "square")
+            {
+                double a = double.Parse(Console.ReadLine());
+                area = a * a;
+                Console.WriteLine("{0:f3}",area);
+            }
+            else if (kindOfFigure == "rectangle")
+            {
+                double a = double.Parse(Console.ReadLine());
+                double b = double.Parse(Console.ReadLine());
+                area = a * b;
+                Console.WriteLine("{0:f3}",area);
+            }
+            else if (kindOfFigure == "circle")
+            {
+                double a = double.Parse(Console.ReadLine());
+                area = Math.PI * (a * a);
+                Console.WriteLine("{0:f3}",area);
+            }
+            else
+            {
+                double a = double.Parse(Console.ReadLine());
+                double b = double.Parse(Console.ReadLine());
+                area = (a * b) / 2;
+                Console.WriteLine("{0:f3}",area);
+            }
+        }
+    }
+}
---
+++ ProgrammingBasicsC#/2lab-ConditionalStatements/Even or Odd/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Even_or_Odd
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Да се напише програма, която чете цяло число въведено от потребителя
+            //и отпечатва на конзолата, дали е четно или нечетно. 
+
+            int number = int.Parse(Console.ReadLine());
+
+            if (number % 2 == 0)
+            {
+                Console.WriteLine("even");
+            
+            }
+            else
+            {
+                Console.WriteLine("odd");
+            }
+        }
+    }
+}
