19c97d573bd6349dd54998cecbe7f20e9cda2441
LyubomirRashkov <lyubomir.rashkov88@gmail.com>
2021-11-04 09:37:49 +0200

    Added code (Second exercise in the course Programming Basics)

---
+++ ProgrammingBasicsC#/2ex-ConditionalStatements/ConditionalStatements/Time +15 Minutes/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Time__15_Minutes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int hour = int.Parse(Console.ReadLine());
+            int minutes = int.Parse(Console.ReadLine());
+
+            if (minutes + 15 < 60)
+            {
+                Console.WriteLine($"{hour}:{minutes + 15}");
+            }
+            if (minutes + 15 >= 60)
+            {
+                minutes = (minutes + 15) - 60;
+                hour = hour + 1;
+                if (hour > 23)
+                {
+                    hour = 0;
+                }
+                Console.WriteLine($"{hour}:{minutes:d2}");
+            }
+        }
+    }
+}
