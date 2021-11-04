6c7257e0d8699d56806d8f23cd8d37b24d180f39
LyubomirRashkov <lyubomir.rashkov88@gmail.com>
2021-11-04 09:39:37 +0200

    Added code (Second exercise in the course Programming Basics)

---
+++ ProgrammingBasicsC#/2ex-ConditionalStatements/ConditionalStatements/World Swimming Record/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace World_Swimming_Record
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double record = double.Parse(Console.ReadLine());
+            double distance = double.Parse(Console.ReadLine());
+            double timePerMeter = double.Parse(Console.ReadLine());
+
+            double timeForWholeDistance = distance * timePerMeter;
+            double waterResistance = Math.Floor(distance / 15);
+
+            double wholeTime = timeForWholeDistance + (waterResistance * 12.5);
+
+            if (wholeTime < record)
+            {
+                Console.WriteLine($"Yes, he succeeded! The new world record is {wholeTime:f2} seconds.");
+            }
+            else
+            {
+                Console.WriteLine($"No, he failed! He was {(wholeTime - record):f2} seconds slower.");
+            }
+        }
+    }
+}
---
+++ ProgrammingBasicsC#/2ex-ConditionalStatements/Scholarships/Scholarships/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace Scholarships
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double income = double.Parse(Console.ReadLine());
+            double GPA = double.Parse(Console.ReadLine());
+            double minimalWage = double.Parse(Console.ReadLine());
+
+            double socialScholarship = 0.35 * minimalWage;
+            double excellentResultsScholarship = GPA * 25;
+
+            if (income < minimalWage && GPA >= 5.50)
+            {
+                if (excellentResultsScholarship >= socialScholarship)
+                {
+                    Console.WriteLine($"You get a scholarship for excellent results {Math.Floor(excellentResultsScholarship)} BGN!");
+                }
+                else
+                {
+                    Console.WriteLine($"You get a social scholarship {Math.Floor(socialScholarship)} BGN!");
+                }
+            }
+
+            else if (income < minimalWage && GPA > 4.50)
+            {
+                Console.WriteLine($"You get a social scholarship {Math.Floor(socialScholarship)} BGN!");
+            }
+
+            else if (GPA >= 5.50)
+            {
+                Console.WriteLine($"You get a scholarship for excellent results {Math.Floor(excellentResultsScholarship)} BGN!");
+            }
+
+            else
+            {
+                Console.WriteLine("You cannot get a scholarship!");
+            }
+        }
+    }
+}
