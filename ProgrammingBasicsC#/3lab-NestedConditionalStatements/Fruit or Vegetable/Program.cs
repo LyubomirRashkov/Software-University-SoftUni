3b8eb01c5fe247be1d4aba0a09defdde40a2a9bc
LyubomirRashkov <lyubomir.rashkov88@gmail.com>
2021-11-05 09:04:42 +0200

    Added code (Third lecture in the course Programming Basics)

---
+++ ProgrammingBasicsC#/3lab-NestedConditionalStatements/Fruit or Vegetable/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace Fruit_or_Vegetable
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string product = Console.ReadLine();
+            string output = "";
+
+            switch (product)
+            {
+                case "banana":
+                case "apple":
+                case "kiwi":
+                case "cherry":
+                case "lemon":
+                case "grapes":
+                    output = "fruit";
+                    break;
+
+                case "tomato":
+                case "cucumber":
+                case "pepper":
+                case "carrot":
+                    output = "vegetable";
+                    break;
+
+                default:
+                    output = "unknown";
+                    break;
+            }
+
+            Console.WriteLine(output);
+
+        }
+    }
+}
