8b64d5bbd229266ca25d5c247964e2db7c311c36
Lyubomir <lyubomir.rashkov88@gmail.com>
2022-12-29 22:11:06 +0200

    Added code

---
+++ ASP.NET_Fundamentals/ProjectsAndSolutionsFromLecturesAndExercises/HTTPProtocol/VerbsIntroduction/VerbsIntroduction/Location.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.ComponentModel.DataAnnotations;
+
+namespace VerbsIntroduction
+{
+    public class Location
+    {
+        [Required]
+        [FromForm(Name = "city")]
+        public string City { get; set; } = null!; 
+    }
+}
