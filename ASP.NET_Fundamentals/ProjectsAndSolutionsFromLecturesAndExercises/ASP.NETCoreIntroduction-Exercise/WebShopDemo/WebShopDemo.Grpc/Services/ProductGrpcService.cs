5d788de1d68f34eda92f46ea036f5ff612b77ddb
Lyubomir <lyubomir.rashkov88@gmail.com>
2023-01-06 16:16:56 +0200

    Added code

---
+++ ASP.NET_Fundamentals/ProjectsAndSolutionsFromLecturesAndExercises/ASP.NETCoreIntroduction-Exercise/WebShopDemo/WebShopDemo.Grpc/Services/ProductGrpcService.cs
@@ -0,0 +1,33 @@
+﻿using Google.Protobuf.WellKnownTypes;
+using Grpc.Core;
+using WebShopDemo.Core.Contracts;
+
+namespace WebShopDemo.Grpc.Services
+{
+    public class ProductGrpcService : Product.ProductBase
+    {
+        private readonly IProductService productService;
+
+        public ProductGrpcService(IProductService productService)
+        {
+            this.productService = productService;
+        }
+
+        public override async Task<ProductList> GetAll(Empty request, ServerCallContext context)
+        {
+            ProductList result = new ProductList();
+
+            var products = await this.productService.GetAll();
+
+            result.Items.AddRange(products.Select(p => new ProductItem()
+            {
+                Name = p.Name,
+                Id = p.Id.ToString(),
+                Price = (double)p.Price,
+                Quantity = p.Quantity
+            }));
+
+            return result;
+        }
+    }
+}
---
+++ ASP.NET_Fundamentals/ProjectsAndSolutionsFromLecturesAndExercises/ASP.NETCoreIntroduction-Exercise/WebShopDemo/WebShopDemo/Controllers/ProductController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebShopDemo.Core.Contracts;
+
+namespace WebShopDemo.Controllers
+{
+    /// <summary>
+    /// Web shop products
+    /// </summary>
+    public class ProductController : Controller
+    {
+        private readonly IProductService productService;
+
+        public ProductController(IProductService productService)
+        {
+            this.productService = productService;
+        }
+
+        /// <summary>
+        /// List all products
+        /// </summary>
+        /// <returns></returns>
+        public async Task<IActionResult> Index()
+        {
+            var products = await this.productService.GetAll();
+
+            ViewData["Title"] = "Products";
+
+            return View(products);
+        }
+    }
+}
